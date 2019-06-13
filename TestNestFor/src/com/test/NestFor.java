57fa46f4072dd06963689cc6d2d6f5a861cc524f
lsqy <lsqy93@gmail.com>
2019-06-13 19:09:56 +0800

    update

--- TestNestFor/src/com/test/NestFor.java
+++ TestNestFor/src/com/test/NestFor.java
@@ -1,10 +1,20 @@
 package com.test;
 
+/**
+ * javadoc≤‚ ‘
+ * @author liushaoqian
+ *
+ */
 public class NestFor {
 	// 1 2 3 4
 	// 2 2 3 4
 	// 3 2 3 4
 	// 4 2 3 4
+	/**
+	 * œ‘ æjavadoc
+	 * @param args
+	 * @return Œﬁ
+	 */
 	public static void main(String[] args) {
 		for(int i = 1; i <= 4; i++) {
 			System.out.print(i+" ");
