e773e170386129852182d82b2124a7f273e6adb3
lsqy <lsqy93@gmail.com>
2019-06-14 22:57:54 +0800

    update

--- TestCar/src/com/lsqy/TestCar.java
+++ TestCar/src/com/lsqy/TestCar.java
@@ -1,15 +1,15 @@
 package com.lsqy;
 
-public class Car {
+public class TestCar {
+	
 	private static int age = 10;
 	public static String name = "xiaoming";
-	
-	public void move() {
-		System.out.println("车可以开");
-	}
-//	System.out.println("");
+
 	public static void main(String[] args) {
+		Car c = new Car();
+		c.move();
 		System.out.println(age);
 		System.out.print(name);
 	}
 }
+
